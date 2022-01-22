2d78f98213039b3ae5c7066ede36fd0b89dfde48
Timo Varila <timo.varila@siili.com>
2022-01-22 19:54:21 +0200

    all

--- WebApplication/5_OAuth/UnicornManager/js/config.js
+++ WebApplication/5_OAuth/UnicornManager/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolClientId: 'COGNITO APPLICATION ID', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'AWS REGION', // e.g. us-east-2
-        authDomainPrefix: 'CONFIGURED COGNITO DOMAIN PREFIX' // wildrydes-sapessi
+        userPoolClientId: 'fjk853f81cs8pd5n3cujd9af6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1', // e.g. us-east-2
+        authDomainPrefix: 'wildrydes-pepe' // wildrydes-sapessi
     },
     api: {
-        invokeUrl: 'API ENDPOINT URI' // e.g. https://xxxxxxxx.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://2qlejwhfy0.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://xxxxxxxx.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
